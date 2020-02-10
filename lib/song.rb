313331a328af293200552e354f2720e5f4964831
Xavier Perez <xperez007@gmail.com>
2020-02-10 17:35:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,25 @@
 class Song 
   attr_accessor :name, :artist, :genre 
 
-
-
-end
-
-
-
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ 
+ninety_nine_problems.name
+   => "99 Problems"
+ 
+ninety_nine_problems.artist
+   => "Jay-Z"
+ 
+ninety_nine_problems.genre
+   => "rap" 
+  
+class count
+  count = 0 
+  count += 1 
+  @@count = count
+  
+class artists
+  artists = .artists
+  @@artists = []
+  
 
 end
